8b2f3caaef15c5429d9592d9d1e1ecaae923798e
DaffaPutraS <daffap434@gmail.com>
2024-12-05 17:32:58 +0700

    create camera,camera2,gender,outfit type page

--- app/src/main/java/com/example/smartfit/view/detection/camera2/Camera2Activity.kt
+++ app/src/main/java/com/example/smartfit/view/detection/camera2/Camera2Activity.kt
@@ -1,4 +1,4 @@
-package com.example.smartfit.view.detection.cameraresult
+package com.example.smartfit.view.detection.camera2
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.smartfit.R
 
-class CameraResultActivity : AppCompatActivity() {
+class Camera2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_camera_result)
+        setContentView(R.layout.activity_camera2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
